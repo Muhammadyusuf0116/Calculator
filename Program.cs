db550711206f38de181caccc19f89394908262f8
Muhammadyusuf <muhammadyusuf120503@gmail.com>
2025-02-12 10:10:11 +0500

    Add Program File

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Julia");
+        }
+    }
+}
